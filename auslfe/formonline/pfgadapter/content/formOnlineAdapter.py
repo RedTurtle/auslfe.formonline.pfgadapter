5277183f79af6dd212f7e6e8b7a4ecbed5cff46f
federica.delia <federica.delia@c6e3998e-5644-4537-8003-e402e8770f0f>
2010-07-09 13:28:48 +0000

    creazione form online

--- auslfe/formonline/pfgadapter/content/formOnlineAdapter.py
+++ auslfe/formonline/pfgadapter/content/formOnlineAdapter.py
@@ -7,6 +7,16 @@ from auslfe.formonline.pfgadapter.config import PROJECTNAME
 from Products.Archetypes.public import Schema, ReferenceField
 from Products.ATReferenceBrowserWidget.ATReferenceBrowserWidget import ReferenceBrowserWidget
 from auslfe.formonline.pfgadapter import formonline_pfgadapterMessageFactory as _
+from Products.CMFCore.utils import getToolByName
+
+try:
+    from plone.i18n.normalizer.interfaces import IUserPreferredURLNormalizer
+    from plone.i18n.normalizer.interfaces import IURLNormalizer
+    URL_NORMALIZER = True
+except ImportError:
+    URL_NORMALIZER = False
+from zope.component import queryUtility
+from Products.Archetypes.config import RENAME_AFTER_CREATION_ATTEMPTS
 
 class FormOnlineAdapter(FormActionAdapter):
     """A form action adapter that will create a FormOnline object (a page)
@@ -39,43 +49,80 @@ class FormOnlineAdapter(FormActionAdapter):
     
     security.declarePrivate('onSuccess')
     def onSuccess(self, fields, REQUEST=None):
-        """ Called by form to invoke custom success processing.
-            return None (or don't use "return" at all) if processing is
-            error-free.
-            
-            Return a dictionary like {'field_id':'Error Message'}
-            and PFG will stop processing action adapters and
-            return back to the form to display your error messages
-            for the matching field(s).
-
-            You may also use Products.PloneFormGen.config.FORM_ERROR_MARKER
-            as a marker for a message to replace the top-of-the-form error
-            message.
-
-            For example, to set a message for the whole form, but not an
-            individual field:
-
-            {FORM_ERROR_MARKER:'Yuck! You will need to submit again.'}
-
-            For both a field and form error:
-
-            {FORM_ERROR_MARKER:'Yuck! You will need to submit again.',
-             'field_id':'Error Message for field.'}
-            
-            Messages may be string types or zope.i18nmessageid objects.                
-        """
-        # fields will be a sequence of objects with an IPloneFormGenField interface
-        self.save_form(fields, REQUEST)
+        """Called by form to invoke custom success processing."""
         
-    def save_form(self, fields, request, **kwargs):
-        """Save the form.
-        """
-        print "Hello World"
-#        self.context.invokeFactory(id=newId,type_name='LPE')
-#        newlpe=getattr(self.context,newId)
-#        newlpe.edit(title=nome,matricola=mtr,nomeequipe=nome,codicereparto=codr,prestazione=prst,
-#                codiceprestazione=codp,branca=br,costoineuro=cost)
-#        newlpe.reindexObject()
+        # fields will be a sequence of objects with an IPloneFormGenField interface
+        self.save_form(fields)
+        
+    def save_form(self, fields):
+        """Creates a FormOnline object and saves form input data in the text field of FormOnline."""
+        
+        container_formonline = self.getFormOnlinePath()
+        
+        mtool = getToolByName(self, 'portal_membership')
+        if mtool.isAnonymousUser():
+            title = _(u'Form completed by an anonymous user')
+        else:
+            username = mtool.getAuthenticatedMember().getUserName()
+            title = _(u'Form completed by %s' % username)
+        
+        formonline_id = self.idCreation(title,container_formonline)
+        if formonline_id:
+            container_formonline.invokeFactory(id=formonline_id,type_name='FormOnline')
+            formonline = getattr(container_formonline,formonline_id)
+            formonline.edit(title=title)
 
+    def idCreation(self, title, container_formonline):
+        """Creates a name for an object like its title."""
+        new_id = self.generateNewId(title)
+        if new_id is None:
+            return False
+
+        # make sure we have an id unique in the parent folder.
+        unique_id = self.findUniqueId(new_id,container_formonline)
+        if unique_id is not None:
+            return unique_id
+
+        return False
+        
+    def generateNewId(self,title):
+        """Suggest an id from title."""
+        # Can't work w/o a title
+        if not title:
+            return None
+
+        # Don't do anything without the plone.i18n package
+        if not URL_NORMALIZER:
+            return None
+
+        if not isinstance(title, unicode):
+            charset = self.getCharset()
+            title = unicode(title, charset)
+
+        request = getattr(self, 'REQUEST', None)
+        if request is not None:
+            return IUserPreferredURLNormalizer(request).normalize(title)
+
+        return queryUtility(IURLNormalizer).normalize(title)
+    
+    def findUniqueId(self, id, parent_folder):
+        """Find a unique id in the parent folder, based on the given id, by
+        appending -n, where n is a number between 1 and the constant
+        RENAME_AFTER_CREATION_ATTEMPTS, set in config.py. If no id can be
+        found, return None."""
+        parent_ids = parent_folder.objectIds()
+        check_id = lambda id, required: id in parent_ids
+        invalid_id = check_id(id, required=1)
+        if not invalid_id:
+            return id
+
+        idx = 1
+        while idx <= RENAME_AFTER_CREATION_ATTEMPTS:
+            new_id = "%s-%d" % (id, idx)
+            if not check_id(new_id, required=1):
+                return new_id
+            idx += 1
+
+        return None
 
 registerATCT(FormOnlineAdapter, PROJECTNAME)
